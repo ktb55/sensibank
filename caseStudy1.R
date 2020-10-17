4f162f2c3ce33ef380e7c384047ef2eca4b5da10
Kalli Buchanan <kalli@hotmail.ca>
2020-10-16 20:08:46 -0400

    Added better graphics and gathered data columns

--- caseStudy1.R
+++ caseStudy1.R
@@ -1,5 +1,11 @@
 ## Libraries
 library(scales)
+library(forecast)
+library(tsutils)
+library(lattice)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
 
 ## Identifying locations where co-branded card is not being used as often
 
@@ -23,4 +29,16 @@ with(sensibank, legend('center', inset = 0,
 ## Trending towards not being used as often: Airport, King Street
 ## Relatively Similar, but trending slightly in favour: Big Street, Mount Road
 dev.copy(png, file = './figures/location_transactions.png')
-dev.off()     
+dev.off()
+
+sensibank_colnames <- colnames(sensibank)[c(1:3,6:7)]
+sensi <- gather(sensibank, Transaction.Type, Number.of.Transactions,-sensibank_colnames)
+sensi <- mutate(sensi,Transaction.Type = factor(Transaction.Type, labels = c("Bank Card", "Off Card")))
+
+transactionPlot <- ggplot(sensi, aes(Transaction.Date, Number.of.Transactions))
+transactionPoints <- transactionPlot + geom_point(aes(color = Transaction.Type), alpha = 0.05) 
+transactionPanels <- transactionPoints +  facet_grid(Store.Location~.) + coord_cartesian(ylim = c(0,200))
+transactionTrends <- transactionPanels + geom_smooth(aes(color = Transaction.Type), method = 'lm', size = 2) 
+transactionTrends + labs(x = "Date", y = "Number of Transactions", color = "Type of Transaction")
+dev.copy(png, file = './figures/location_transactions2.png', width  = 1000, height = 1000)
+dev.off()
