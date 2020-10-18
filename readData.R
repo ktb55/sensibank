d9b58276df6227d3ad754db674374765d3b5954f
Kalli Buchanan <kalli@hotmail.ca>
2020-10-17 23:44:57 -0400

    Renamed Working.Day to Day.Type

--- readData.R
+++ readData.R
@@ -5,14 +5,17 @@
 ################################################################################
 
 ## Libraries
-library("dplyr")
+library(dplyr)
 
 ## Read in data
 sensibank <- read.csv("./data/sensibank-aggregate-data.csv", header = TRUE) %>%
                 select(-Weekday) %>%
+                rename(Day.Type = Working.Day) %>%
                 mutate(Transaction.Date = as.Date(Transaction.Date), 
-                       Working.Day = factor(Working.Day, levels = c('H', 'W'), 
+                       Day.Type = factor(Day.Type, levels = c('H', 'W'), 
                                             labels = c('Holiday', 'Work Day')),
                        Number.of.Bank.Card.Transactions = as.integer(Number.of.Bank.Card.Transactions),
                        Off.Card.Spend = as.numeric(Off.Card.Spend))
+                
+
 
