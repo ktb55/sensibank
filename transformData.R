95797ef01eee643f2f025897e6658760007237e7
Kalli Buchanan <kalli@hotmail.ca>
2020-10-19 14:25:19 -0400

    New data frames added

--- transformData.R
+++ transformData.R
@@ -21,4 +21,28 @@ sensiData <- merge(sensiTransactions,sensiSpend, by = c("Store.Location", "Trans
   arrange(Store.Location, Transaction.Date)
 
 ## Check for NAs
-any(is.na(sensiData)) # Returns FALSE, thus no NAs
+any(is.na(sensiData)) ## Returns FALSE, thus no NAs
+
+## Add column to data for average spend based on transaction number
+any(sensiData$Total.Transactions == 0) ## TRUE, some total transactions = 0
+sum(sensiData$Total.Spend[which(sensiData$Total.Transactions == 0)]) ## Output = 0, when trans = 0, spend = 0
+sensiDataAvg <- mutate(sensiData, Average.Spend = ifelse(Total.Transactions == 0, 0, round(Total.Spend/Total.Transactions, digits = 2)))
+
+any(is.na(sensiDataAvg)) # Returns FALSE
+
+## Create data frame to check correlations by setting factor columns to numeric
+sensiDataCorr <- sensiDataAvg %>%
+  select(-Transaction.Date) %>%
+  mutate(Store.Location = factor(Store.Location)) %>%
+  mutate(Store.Location = as.numeric(Store.Location), Day.Type = as.numeric(Day.Type), Card.Type = as.numeric(Card.Type))
+cor(sensiDataCorr) 
+
+## Shows correlation between:
+## - Store Location and Transactions
+## - Store Location and Spend
+## - Day Type and Average Spend
+## - Card Type and Transactions
+## - Card Type and Spend
+## - Card Type and Average Spend
+
+saveRDS(sensiDataAvg, file = "./sensibank_dashboard/sensiDataAvg.rds")
